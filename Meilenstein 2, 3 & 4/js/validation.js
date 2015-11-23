12648a6abc19420eac44ff1bf6ddc7ac7fd4a7c1
SonnyBoy3428 <1412293@stud.hs-mannheim.de>
2015-11-23 16:07:43 +0100

    Changed: validation.js, Kommentare hinzugefügt, Task 3, Dustin Noah

--- Meilenstein 2, 3 & 4/js/validation.js
+++ Meilenstein 2, 3 & 4/js/validation.js
@@ -1,6 +1,13 @@
+/**
+ * Benachrichtigung, wenn fehlerhafte Eingaben vorliegen
+ */
 var alertMessage = "Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben";
 
+/**
+ * Funktion in der die zu prüfenden HTML-Elemente extrahiert und auf fehlerhaften Werten überprüft werden
+ */
 function validation() {
+    //Extrahierung der einzelnen HTML-Elementen
     var vorname = document.player_entry.vorname;
     var name = document.player_entry.name;
     var verein = document.player_entry.verein;
@@ -9,6 +16,7 @@ function validation() {
     var number = document.player_entry.number;
     var jahr = document.player_entry.jahr;
 
+    //Prüfung auf Richtigkeit der Werte
     if (validateYear(jahr)&validateNumber(number)&validateText(acoach) & validateText(hcoach) & validateText(verein) & validateText(name) & validateText(vorname)) {
         return true;
     } else {alert(alertMessage);
@@ -16,6 +24,9 @@ function validation() {
            }
 }
 
+/**
+ * Funktion zum Prüfen der Textfelder auf invalide Werte
+ */
 function validateText(text) {
     var validChars = /^[a-zß-üA-ZÄ-Ü]+$/;
     if (text.value.match(validChars)) {
@@ -26,6 +37,9 @@ function validateText(text) {
     }
 }
 
+/**
+ * Funktion zum Prüfen der Nummernfelder auf invalide Werte
+ */
 function validateNumber(number) {
     if (number.value>=4&&number.value<=15) {
         number.style.removeProperty("border");
@@ -35,6 +49,9 @@ function validateNumber(number) {
     }
 }
 
+/**
+ * Funktion zum Prüfen des Feldes für den Jahrgang auf invalide Werte
+ */
 function validateYear(year) {
     if (year.value>=0&&year.value<=2015&&year.value!="") {
         year.style.removeProperty("border");
@@ -44,6 +61,9 @@ function validateYear(year) {
     }
 }
 
+/**
+ * Funktion, die das Verhalten des Eingabefelder, bei auftretenden Fehlern bestimmt
+ */
 function errorHandler(text) {
     text.style.borderColor = "red";
     text.focus();
