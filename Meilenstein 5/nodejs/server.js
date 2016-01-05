0725fc7650b766ba32b444679eb3b5d75fe7ca8e
SonnyBoy3428 <1412293@stud.hs-mannheim.de>
2016-01-05 20:57:24 +0100

    Changed: server.js, Formulardaten werden in gewünschter Reihenfolge in Array gefüllt, Task 5.2, Dustin Noah

--- Meilenstein 5/nodejs/server.js
+++ Meilenstein 5/nodejs/server.js
@@ -5,9 +5,24 @@ var querystring = require('querystring');
 http.createServer(function (request, response){
 	console.log('User connected to Server');
 
+	//Array für die URL-Parameter
 	var requestQueryParameters;
-	requestQueryParameters = querystring.parse(request.url);
+
+	Array für die Formulardaten
+	var parameterList = new Array();
+
+
+	requestQueryParameters = querystring.parse(request.url.replace(/^.*\?/, ''));
 	console.log(requestQueryParameters);
+
+	//Array mit den Formulardaten befüllen
+	parameterList[0] = requestQueryParameters['vorname'] + ' ' + requestQueryParameters['name'];
+	parameterList[1] = requestQueryParameters['jahr'];
+	parameterList[2] = requestQueryParameters['hcoach'];
+	parameterList[3] = requestQueryParameters['acoach'];
+	parameterList[4] = requestQueryParameters['position'];
+	parameterList[5] = requestQueryParameters['number'];
+	console.log(parameterList);
 
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end('Sie haben sich erfolgreich auf den WebServer mit der Url <192.168.178.28:3000> verbunden');
