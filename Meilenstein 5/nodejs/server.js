d2cec462afad449c3da1903eb8a363595ae1291f
SonnyBoy3428 <1412293@stud.hs-mannheim.de>
2016-01-05 23:13:25 +0100

    Changed: server.js, Kommentare hinzugefügt, Task 5.2, Dustin Noah

--- Meilenstein 5/nodejs/server.js
+++ Meilenstein 5/nodejs/server.js
@@ -1,47 +1,47 @@
 /**
- * 
+ * Variable, die das http-Modul importiert
  */
 var http = require('http');
 
 /**
- * 
+ * Variable, die das querystring-Modul importiert
  */
 var querystring = require('querystring');
 
 /**
- * 
+ * Variable, die das fs-Modul importiert
  */
 var fs = require('fs');
 
 /**
- * 
+ * Variable, die das os-Modul importiert (beinhaltet betriebssystemspezifische Formatierungsmöglichkeiten)
  */
 var os = require("os");
 
-//
+//Erstellung des Servers, mit Verhalten bei Requestverarbeitung
 http.createServer(function (request, response){
 	console.log('User connected to Server');
 
 	/**
- 	* 
+ 	* Variable für das Parsen des Querystrings
  	*/
 	var requestQueryParameters;
 
 	/**
- 	* 
+ 	* Variable für den Inhalt der Formularelemente in der Richtigen Reihenfolge
  	*/
 	var parameterList;
 
-	//
+	//Parsen der Request-URL in einen Querystring
 	requestQueryParameters = querystring.parse(request.url.replace(/^.*\?/, ''));
 
-	//
+	//Füllen des Strings mit Formulardaten in der richtigen Reihenfolge
 	parameterList = requestQueryParameters['vorname'] + ' ' + requestQueryParameters['name'] + ', ' 
 	+ requestQueryParameters['jahr'] + ', ' + requestQueryParameters['hcoach'] + ', ' 
 	+ requestQueryParameters['acoach'] + ', ' + requestQueryParameters['position'] + ', ' 
 	+ requestQueryParameters['number'] + os.EOL;
 
-	//
+	//Anfügen der Formulardaten an die Textdatei
 	fs.appendFile('form.txt', parameterList);
 
 	response.writeHead(200, {'Content-Type': 'text/plain'});
