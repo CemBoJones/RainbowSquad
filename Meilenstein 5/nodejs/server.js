de1286b3ea755f083b246b1b909ca1a5f4b6dc8f
SonnyBoy3428 <1412293@stud.hs-mannheim.de>
2016-01-05 21:27:09 +0100

    Changed: server.js, fs-Modul hinzugefügt, appendFile-Methode hinzugefügt, parameterList-Array in String umgeändert, Task 5.2, Dustin Noah
    Added: form.tx, vorgegebene Textdatei zur Aufname von Formulardaten,
    Task 5.2, Dustin Noah
    
    Changed: form.tx, ergibt sich aus Testversuche (ERFOLG!), Task 5.2,
    Dustin Noah

--- Meilenstein 5/nodejs/server.js
+++ Meilenstein 5/nodejs/server.js
@@ -1,28 +1,28 @@
 var http = require('http');
 var querystring = require('querystring');
+var fs = require('fs');
 
 
 http.createServer(function (request, response){
 	console.log('User connected to Server');
 
-	//Array für die URL-Parameter
 	var requestQueryParameters;
-
-	Array für die Formulardaten
-	var parameterList = new Array();
-
+	var parameterList;
 
 	requestQueryParameters = querystring.parse(request.url.replace(/^.*\?/, ''));
-	console.log(requestQueryParameters);
 
-	//Array mit den Formulardaten befüllen
-	parameterList[0] = requestQueryParameters['vorname'] + ' ' + requestQueryParameters['name'];
-	parameterList[1] = requestQueryParameters['jahr'];
+	parameterList = requestQueryParameters['vorname'] + ' ' + requestQueryParameters['name'] + ', ' 
+	+ requestQueryParameters['jahr'] + ', ' + requestQueryParameters['hcoach'] + ', ' 
+	+ requestQueryParameters['acoach'] + ', ' + requestQueryParameters['position'] + ', ' 
+	+ requestQueryParameters['number'];
+	
+	/*parameterList[1] = requestQueryParameters['jahr'];
 	parameterList[2] = requestQueryParameters['hcoach'];
 	parameterList[3] = requestQueryParameters['acoach'];
 	parameterList[4] = requestQueryParameters['position'];
-	parameterList[5] = requestQueryParameters['number'];
-	console.log(parameterList);
+	parameterList[5] = requestQueryParameters['number'];*/
+
+	fs.appendFile('form.txt', parameterList);
 
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end('Sie haben sich erfolgreich auf den WebServer mit der Url <192.168.178.28:3000> verbunden');
