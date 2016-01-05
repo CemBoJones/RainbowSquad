952e22cbbda3b39af36b11b3a0f284f01897a6ae
SonnyBoy3428 <1412293@stud.hs-mannheim.de>
2016-01-05 20:37:59 +0100

    Changed: server.js, Querystring hinzugefügt, geparst und getestet, Task 5.2, Dustin Noah

--- Meilenstein 5/nodejs/server.js
+++ Meilenstein 5/nodejs/server.js
@@ -1,7 +1,14 @@
 var http = require('http');
+var querystring = require('querystring');
+
 
 http.createServer(function (request, response){
 	console.log('User connected to Server');
+
+	var requestQueryParameters;
+	requestQueryParameters = querystring.parse(request.url);
+	console.log(requestQueryParameters);
+
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end('Sie haben sich erfolgreich auf den WebServer mit der Url <192.168.178.28:3000> verbunden');
 }).listen(3000, '192.168.178.28');
